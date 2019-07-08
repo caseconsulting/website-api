951251f7cadbcfaff271016c0538b8724b3d5af5
Sneha Kuchipudi <skuchipudi@consultwithcase.com>
2019-07-08 10:25:01 -0400

    621-sns-email-question-mark: removed question marks for publishes sns topic and cleaned up code

--- apply/index.js
+++ apply/index.js
@@ -15,21 +15,6 @@ function _getSNS() {
 
 function _parseData(body) {
   let data = {};
-  /*
-  if (_.get(body, 'firstName') && !_.isEmpty(body.firstName)) data.firstName = body.firstName;
-  if (_.get(body, 'lastName') && !_.isEmpty(body.lastName)) data.lastName = body.lastName;
-  if (_.get(body, 'email') && !_.isEmpty(body.email)) data.email = body.email;
-  if (_.get(body, 'jobTitles') && !_.isEmpty(body.jobTitles)) data.jobTitles = _.join(body.jobTitles);
-  if (_.get(body, 'otherJobTitle') && !_.isEmpty(body.otherJobTitle)) data.otherJobTitle = body.otherJobTitle;
-  if (_.get(body, 'hearAboutUs') && !_.isEmpty(body.hearAboutUs)) data.hearAboutUs = _.join(body.hearAboutUs);
-  if (_.get(body, 'referralHearAboutUs') && !_.isEmpty(body.referralHearAboutUs)) {
-    data.referralHearAboutUs = body.referralHearAboutUs;
-  }
-  if (_.get(body, 'otherHearAboutUs') && !_.isEmpty(body.otherHearAboutUs)) {
-    data.otherHearAboutUs = body.otherHearAboutUs;
-  }
-  if (_.get(body, 'fileNames') && !_.isEmpty(body.fileNames)) data.fileNames = _.join(body.fileNames);
-  */
 
   if (_.get(body, 'firstName') && !_.isEmpty(body.firstName)) {
     data.firstName = body.firstName;
@@ -113,7 +98,7 @@ async function _publish(id, data) {
     Job Title(s): ${data.jobTitles.replace(/,/g, ', ')}`;
   if (data.otherJobTitle) {
     Message += `
-    Other Job Titles?: ${data.otherJobTitle}`;
+    Other Job Titles: ${data.otherJobTitle}`;
   }
   if (data.hearAboutUs && data.otherHearAboutUs) {
     Message += `
@@ -124,7 +109,7 @@ async function _publish(id, data) {
   }
   if (data.referralHearAboutUs) {
     Message += `
-    Employee Referral?: ${data.referralHearAboutUs}`;
+    Employee Referral: ${data.referralHearAboutUs}`;
   }
   Message += `
     Resume Filenames: ${data.fileNames.replace(/,/g, ', ')}`;
